904d2dfdd39a10e42dc5955305cbe8e6ea22e28b
Sandra Bergstrom <bergstrom_sandra@hotmail.com>
2023-04-07 15:13:48 +0000

    turn on debug

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["django-blog-sb-ic.herokuapp.com", "localhost", "8000-sandrabergstrom-django-b-juw3n6gnyf.us2.codeanyapp.com"]
 
